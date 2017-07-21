bad4d26a81cd19b7843616fcbe2a688863f866ae
MyMacMini <macmini@mymacminidemini.lan>
2017-07-21 21:10:02 +0800

    Initial Commit

---
+++ commandLine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  commandLine
+//
+//  Created by MyMacMini on 2017/7/21.
+//  Copyright © 2017年 MyMacMini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
